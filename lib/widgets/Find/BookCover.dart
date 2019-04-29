dcb769897f19ffff85a50086878830d96e20df62
ming.xie <ming.xie@naoxuejia.com>
2019-04-29 15:08:26 +0800

    发现页面布局

--- lib/widgets/Find/BookCover.dart
+++ lib/widgets/Find/BookCover.dart
@@ -5,43 +5,100 @@ import 'package:flutter_book/widgets/Find/ReadUser.dart';
 class BookCover extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            width: 196.0,
-            height: 290.0,
+    final String _avatarUrl =
+        'http://admin.soscoon.com/uploadImages/9b7c69ea2c754b0e0a8f3604f4ca4ad1d60e3c74.jpg';
+
+    return Stack(children: <Widget>[
+      Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 196.0,
+              height: 290.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'http://admin.soscoon.com/uploadImages/21dc88543e012af8e8d55440b9cbcf8b379c4206.png'))),
+            ),
+            Container(
+              width: 168.0,
+              margin: EdgeInsets.symmetric(vertical: 24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Empowering children and youth',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color(AppColors.fontColor), fontSize: 18.0),
+                  ),
+                  SizedBox(
+                    height: 2.0,
+                  ),
+                  Text(
+                    'Michael, Jost',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color(AppColors.fontColorGray), fontSize: 14.0),
+                  )
+                ],
+              ),
+            ),
+            ReadUser()
+          ],
+        ),
+      ),
+      Positioned(
+        right: 0.0,
+        bottom: 35.0,
+        child: Container(
+            height: 42.0,
+            width: 120.0,
+            margin: EdgeInsets.only(top: 4.0, bottom: 4.0, left: 4.0),
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: NetworkImage(
-                        'http://admin.soscoon.com/uploadImages/21dc88543e012af8e8d55440b9cbcf8b379c4206.png'))),
-          ),
-          Container(
-            width: 168.0,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+                color: Color(AppColors.themeColorGray),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20.0))),
+            child: Row(
               children: <Widget>[
-                Text(
-                  'Empowering children and youth',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color(AppColors.fontColor), fontSize: 18.0),
+                Container(
+                  width: 34.0,
+                  height: 34.0,
+                  margin: EdgeInsets.only(right: 4.0),
+                  decoration: BoxDecoration(
+                      color: Color(AppColors.themeColorGray),
+                      borderRadius: BorderRadius.all(Radius.circular(40.0)),
+                      image: DecorationImage(image: NetworkImage(_avatarUrl))),
                 ),
-                Text(
-                  'Michael, Jost',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color(AppColors.fontColorGray), fontSize: 18.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '小明同学',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Color(AppColors.fontColor), fontSize: 14.0),
+                      ),
+                      Text(
+                        "Recommended",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Color(AppColors.fontColorGray),
+                            fontSize: 12.0),
+                      )
+                    ],
+                  ),
                 )
               ],
-            ),
-          ),
-          ReadUser()
-        ],
-      ),
-    );
+            )),
+      )
+    ]);
   }
 }
--- lib/widgets/Find/ReadUser.dart
+++ lib/widgets/Find/ReadUser.dart
@@ -5,6 +5,8 @@ const List<String> _avatarUrl = [
   'http://admin.soscoon.com/uploadImages/2bdcbd37d2fae6874cce4bba45d14573630925b1.jpeg',
   'http://admin.soscoon.com/uploadImages/9b7c69ea2c754b0e0a8f3604f4ca4ad1d60e3c74.jpg',
   'http://admin.soscoon.com/uploadImages/936d36286e1e663add46b8e472ec5f20c03d5270.jpeg',
+  'http://admin.soscoon.com/uploadImages/a705454629b1c61a6e606ab016ee8989cceba6ff.jpeg',
+  'http://admin.soscoon.com/uploadImages/a705454629b1c61a6e606ab016ee8989cceba6ff.jpeg',
   'http://admin.soscoon.com/uploadImages/a705454629b1c61a6e606ab016ee8989cceba6ff.jpeg'
 ];
 
@@ -15,19 +17,21 @@ class ReadUser extends StatelessWidget {
       children: <Widget>[
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: _avatarUrl.map((url) {
+          children: _avatarUrl.sublist(0, 4).map((url) {
             return Container(
               margin: EdgeInsets.symmetric(horizontal: 3.0),
               width: 34.0,
               height: 34.0,
               decoration: BoxDecoration(
+                  color: Color(AppColors.themeColorGray),
                   image: DecorationImage(image: NetworkImage(url)),
                   borderRadius: BorderRadius.all(Radius.circular(40.0))),
             );
           }).toList(),
         ),
+        SizedBox(height: 16.0),
         Text(
-          '457 people are reading',
+          '457 位用户已学习',
           style: TextStyle(color: Color(AppColors.fontColor)),
         )
       ],
