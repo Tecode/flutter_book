afaefe273c3523152c404e138ac14a55027eb874
Tecode <283731869@qq.com>
2019-06-04 08:38:13 +0800

    登录页

--- lib/containers/Login.dart
+++ lib/containers/Login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_book/containers/Entrance.dart';
+import 'package:flutter_book/routers/application.dart';
+import 'package:fluro/fluro.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -13,15 +15,31 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         title: Text("登录"),
       ),
-      body: MaterialButton(
-        onPressed: () {
-          // 禁止跳回到之前登录的路由
-          Navigator.of(context).pushAndRemoveUntil(
-              new MaterialPageRoute(builder: (context) => Entrance()),
-              (route) => route == null);
-        },
-        color: Colors.white,
-        child: Text("跳过"),
+      body: Row(
+        children: <Widget>[
+          MaterialButton(
+            onPressed: () {
+              // 禁止跳回到之前登录的路由
+              Navigator.of(context).pushAndRemoveUntil(
+                  new MaterialPageRoute(builder: (context) => Entrance()),
+                  (route) => route == null);
+            },
+            color: Colors.white,
+            child: Text("跳过"),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Application.router.navigateTo(
+                context,
+                "/register",
+                transition: TransitionType.native,
+                // transitionDuration: const Duration(milliseconds: 300),
+              );
+            },
+            color: Colors.blue,
+            child: Text("注册"),
+          )
+        ],
       ),
     );
   }
--- lib/containers/Register.dart
+++ lib/containers/Register.dart
@@ -6,10 +6,69 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _emailFilter = TextEditingController();
+  final TextEditingController _passwordFilter = TextEditingController();
+  String _email = "";
+  String _password = "";
+
+  _RegisterState() {
+    _emailFilter.addListener(_emailListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _emailListen() {
+    if (_emailFilter.text.isEmpty) {
+      _email = "";
+    } else {
+      _email = _emailFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("注册"),
+      appBar: AppBar(
+        title: Text("注册"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: TextField(
+                controller: _emailFilter,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+            ),
+            Container(
+              child: TextField(
+                controller: _passwordFilter,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+// 事件
+  void _loginPressed() {
+    print('The user wants to login with $_email and $_password');
+  }
+
+  void _createAccountPressed() {
+    print('The user wants to create an accoutn with $_email and $_password');
+  }
+
+  void _passwordReset() {
+    print("The user wants a password reset request sent to $_email");
   }
 }
