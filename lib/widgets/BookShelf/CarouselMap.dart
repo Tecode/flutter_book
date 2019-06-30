da369cecd2cb5e98ade4f08ccac5904b3ac078f9
Tecode <283731869@qq.com>
2019-06-30 23:51:34 +0800

    优化轮播图

--- lib/widgets/BookShelf/CarouselMap.dart
+++ lib/widgets/BookShelf/CarouselMap.dart
@@ -6,39 +6,25 @@ class CarouselMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CarouselSlider(
-      height: 210.0,
+      height: 200.0,
       autoPlay: true,
-      // enlargeCenterPage: true,
-      onPageChanged: (int index) {},
+      enlargeCenterPage: true,
+      aspectRatio: 2.0,
+      onPageChanged: (int index) {
+        print(index);
+      },
       viewportFraction: 0.36,
       items: [1, 2, 3, 4, 5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width - 90.0,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                // decoration: BoxDecoration(color: Colors.amber),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      width: 116.0,
-                      height: 170,
-                      margin: EdgeInsets.only(bottom: 6.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  'http://admin.soscoon.com/uploadImages/763229ced887b7e1e61a20aa1b14b42425c01e01.png')),
-                          color: Color(AppColors.themeColorGray)),
-                    ),
-                    Text(
-                      'Sortir a Cannes',
-                      style: TextStyle(
-                          color: Color(AppColors.fontColor), fontSize: 13.0),
-                    )
-                  ],
-                ));
-          },
+        return Container(
+          margin: EdgeInsets.all(5.0),
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            child: Image.network(
+              'http://admin.soscoon.com/uploadImages/763229ced887b7e1e61a20aa1b14b42425c01e01.png',
+              fit: BoxFit.cover,
+              width: 600.0,
+            ),
+          ),
         );
       }).toList(),
     );
