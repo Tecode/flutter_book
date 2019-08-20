1eb625eaf6aca5ec2f81fe3746a05a4402dfaadf
Tecode <283731869@qq.com>
2019-08-20 21:18:27 +0800

    修改了刷新的组件

--- lib/containers/FirstScreen.dart
+++ lib/containers/FirstScreen.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_book/widgets/FirstScreen/FirstScreenContent.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_book/stores/homeStore.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_book/helpers/constants.dart';
+import 'package:flutter_book/widgets/common/Hearder.dart';
 
 class FirstScreen extends StatefulWidget {
   @override
@@ -11,10 +15,14 @@ class FirstScreen extends StatefulWidget {
 class _FirstScreenState extends State<FirstScreen>
     with AutomaticKeepAliveClientMixin {
   HomeStore homeStore;
+  EasyRefreshController _controller;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // 下拉刷新
+    _controller = EasyRefreshController();
+    // 发送请求
     Future.delayed(Duration.zero, () {
       homeStore = Provider.of<HomeStore>(this.context);
       homeStore.getData();
@@ -23,9 +31,28 @@ class _FirstScreenState extends State<FirstScreen>
 
   @override
   Widget build(BuildContext context) {
-    // final homeStore = Provider.of<HomeStore>(context);
-    // homeStore.getData();
-    return FirstScreenContent();
+    return EasyRefresh(
+      enableControlFinishRefresh: false,
+      // header: MaterialHeader(),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                left: Constants.pageMargin,
+                right: Constants.pageMargin,
+                top: 20.0,
+                bottom: 40),
+            child: FirstScreenContent(),
+          ),
+        ],
+      ),
+      onRefresh: () async {
+        await Future.delayed(Duration(seconds: 2), () {
+          // _controller.resetLoadState();
+          _controller.finishRefresh(success: true);
+        });
+      },
+    );
   }
 
   @override
--- lib/widgets/FirstScreen/FirstScreenContent.dart
+++ lib/widgets/FirstScreen/FirstScreenContent.dart
@@ -5,47 +5,17 @@ import 'package:flutter_book/widgets/FirstScreen/Recommend.dart';
 import 'package:flutter_book/widgets/FirstScreen/HotProduct.dart';
 import 'package:flutter_book/widgets/FirstScreen/SearchNouns.dart';
 import 'package:flutter_book/widgets/FirstScreen/PopularBooks.dart';
-import 'package:flutter_book/helpers/constants.dart';
-import 'package:flutter_book/widgets/common/PullRefresh.dart';
 
 class FirstScreenContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PullRefresh(
-        showChildOpacityTransition: false,
-        onRefresh: () {
-          final Completer<void> completer = Completer<void>();
-          Timer(const Duration(seconds: 1), () {
-            completer.complete();
-          });
-          return completer.future.then<void>((_) {
-            // _scaffoldKey.currentState?.showSnackBar(SnackBar(
-            //     content: const Text('Refresh complete'),
-            //     action: SnackBarAction(
-            //         label: 'RETRY',
-            //         onPressed: () {
-            //           _refreshIndicatorKey.currentState.show();
-            //         })));
-          });
-        },
-        child: ListView(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(
-                  left: Constants.pageMargin,
-                  right: Constants.pageMargin,
-                  top: 20.0,
-                  bottom: 40),
-              child: Column(
-                children: <Widget>[
-                  Recommend(),
-                  HotProduct(),
-                  SearchNouns(),
-                  PopularBooks()
-                ],
-              ),
-            ),
-          ],
-        ));
+    return Column(
+      children: <Widget>[
+        Recommend(),
+        HotProduct(),
+        SearchNouns(),
+        PopularBooks()
+      ],
+    );
   }
 }
